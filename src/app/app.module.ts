1c9b6098005bad8dbb3ec9fe694fcf6c93af39c5
Kristen Kulha and Tyler Miller <kristen@>
2017-11-01 15:44:01 -0700

    Add clarity and basic html setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ClarityModule } from "clarity-angular";
+
 
 @NgModule({
   declarations: [
@@ -11,8 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    routing,
+    HttpModule,
+    AngularFireModule.initializeApp(masterFirebaseConfig),
+    AngularFireDatabaseModule,
     FormsModule,
-    HttpModule
+    ClarityModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
